ce230d7595d9b8115bdcbdb76fde44e57a3f3039
leehyeokju <hyeogjui4@gmail.com>
2022-09-24 20:55:13 +0900

    mysql -> mariadb

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationListener;
+@Slf4j
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication  implements ApplicationListener<ApplicationStartedEvent> {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void onApplicationEvent(ApplicationStartedEvent event) {
+		log.info("spring start!!!");
+	}
 }
